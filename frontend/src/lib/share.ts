13e7e6df816324b0c11db13cb03e6c77e43c8929
yuchan <oyc0401@gmail.com>
2026-02-02 17:50:29 +0900

    공유 경로 수정

--- frontend/src/lib/share.ts
+++ frontend/src/lib/share.ts
@@ -9,9 +9,11 @@ const scheme =
 
 export async function handleShare(roomId: string) {
   const tossLink = await getTossShareLink(
-    `${scheme}://findtime/room?id=${roomId}`,
+    `${scheme}://findtime/rooms/${roomId}&_deploymentId=019c1d7d-e97c-774a-96af-4080d991ac26`,
     "https://static.toss.im/icons/png/4x/icon-share-dots-mono.png",
   );
 
   await share({ message: tossLink });
 }
+// intoss-private://findtime?_deploymentId=019c1d7d-e97c-774a-96af-4080d991ac26
+// intoss-private://findtime/rooms?id=axTu4U1j&_deploymentId=019c1d7d-e97c-774a-96af-4080d991ac26
