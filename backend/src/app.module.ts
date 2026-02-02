4ee79a5a822ee825c978fabc926bfe2a6ec100b1
yuchan <oyc0401@gmail.com>
2026-02-02 17:00:34 +0900

    헬스체크

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { PrismaModule } from "./prisma/prisma.module";
 import { RoomsModule } from "./rooms/rooms.module";
+import { HealthController } from "./health.controller";
 
 @Module({
   imports: [PrismaModule, RoomsModule],
+  controllers: [HealthController],
 })
 export class AppModule {}
